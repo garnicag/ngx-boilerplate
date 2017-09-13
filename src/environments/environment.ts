6cd2bb049318bec0fac616a4d052acf3cb47c36b
Gabriel Garnica <gabriel.garnica@rokk3rlabs.com>
2017-09-13 18:01:38 -0500

    Environment and template changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,10 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  authConfig: {
+    authDomain: 'ngrxfire-b8208.firebaseapp.com',
+    apiKey: 'AIzaSyBezwrBsweRsZJ5cSwkOo1rDeSZf7Mxct0',
+    clientId: '2'
+  }
 };
